93b0f35dcb8dfdd11e75d3899e5811821cb793b2
张知法 <1004838016@qq.com>
2017-10-25 09:49:08 +0800

    1

--- WeiXinBLL/Class1.cs
+++ WeiXinBLL/Class1.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace WeiXinBLL
 {
-    public class Class1
+   public class Class1
     {
+        public int test()
+        {
+            return 1;
+        }
+        public string test()
+        {
+            return "";
+        }
+        public void test()
+        {
+
+        }
     }
 }
