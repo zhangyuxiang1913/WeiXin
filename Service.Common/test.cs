7fe932b2054a5a4679981a9610d9c938119c19af
张知法 <1004838016@qq.com>
2017-10-25 09:25:54 +0800

    提交

--- Service.Common/test.cs
+++ Service.Common/test.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Service.Common
 {
-    class test
+   public class test
     {
+       public int test()
+       {
+           return 1;
+       }
+       public string test()
+       {
+           return "";
+       }
+       public void test()
+       {
+           
+       }
     }
 }
